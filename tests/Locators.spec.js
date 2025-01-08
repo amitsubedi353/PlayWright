b370ba3f89f35fe7b4c02c3a1bc25eafc0bb4cd2
amitsubedi353 <amitsubedi353@gmail.com>
2025-01-08 21:24:02 +0545

    locators update

--- tests/Locators.spec.js
+++ tests/Locators.spec.js
@@ -1,15 +1,25 @@
-//const {test, expect } = require("@playwright/test");
-import {test, expect} from 'playwright/test'
+//const {test, expect} = require('@playwright/test')
+import {test, expect} from '@playwright/test'
 
-test('Locators', async ({page})=>{
+test('Locators', async ({page}) =>{
 
-    await page.goto("https://app.develop.krispchats.com/auth/login")
+    await page.goto('https://demoblaze.com/index.html');
 
-    //await page.click("data-test-id=login-btn")
+    //await page.locator('id=login2').click();
+    await page.click('id=login2');
 
-    await page.locator("data-test-id=email-test-field").fill("Amit")
-    await page.locator("input data-test-id=password-test-field").fill("Subedi")
-    await page.locator("data-test-id=login-btn").click()
+    //await page.locator('#loginusername').fill('django');
+    await page.fill('#loginusername','django0');
+    //await page.type('#loginusername');
 
+    await page.fill("input[id='loginpassword']",'test@123');
+
+    await page.click("//button[normalize-space()='Log in']");
+
+    const logoutlink = await page.locator("//a[normalize-space()='Log out']");
+
+    await expect(logoutlink).toBeVisible();
+
+    await page.close();
 
 })
